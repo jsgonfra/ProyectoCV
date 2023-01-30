adaeafa719b8cd0a88318ba4fce9ab64865edb59
Jorge Sebastian Gonzaez Franze <jsgonfra@gmail.com>
2023-01-30 10:04:59 -0300

    ultima subida

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -1,38 +1,45 @@
-/*
-document.getElementById("boton").onmouseover = function() {
-    console.log("capturamos el evento click")
-    document.getElementById("demo").innerHTML = "Estoy probando JS en HTML"
-}
-
-document.getElementById("boton").onmouseleave = function() {
-    document.getElementById("demo").innerHTML = "Estamos aprendiendo JavaScript"
-}
-
-
-function iconosdatos (id_icono, texto) {
-    document.getElementById("id_icono").onmouseover = function() {
-        console.log("capturamos el evento click")
-        document.getElementById("demo").innerHTML = texto
-    }
-}
-
-function iconos_datos (person_img, Carlos)
-*/
 
 document.getElementById("person_img").onmouseover = function() {
     console.log("capturamos el evento click")
     document.getElementById("dato").innerHTML = "Carlos Sanches"
+    document.getElementById("person_img").style.opacity = "1.0";
 }
+
+document.getElementById("person_img").onmouseleave = function() {
+    console.log("capturamos el evento click")
+    document.getElementById("person_img").style.opacity = "0.5";
+}
+
 
 document.getElementById("mail_img").onmouseover = function() {
     console.log("capturamos el evento click")
     document.getElementById("dato").innerHTML = "Carlos.Sanches@gmail.com"
+    document.getElementById("mail_img").style.opacity = "1.0";
 }
+
+document.getElementById("mail_img").onmouseleave = function() {
+    console.log("capturamos el evento click")
+    document.getElementById("mail_img").style.opacity = "0.5";
+}
+
 document.getElementById("phone_img").onmouseover = function() {
     console.log("capturamos el evento click")
     document.getElementById("dato").innerHTML = "0800-0000-0000"
+    document.getElementById("phone_img").style.opacity = "1.0";
 }
+
+document.getElementById("phone_img").onmouseleave = function() {
+    console.log("capturamos el evento click")
+    document.getElementById("phone_img").style.opacity = "0.5";
+}
+
 document.getElementById("address_img").onmouseover = function() {
     console.log("capturamos el evento click")
     document.getElementById("dato").innerHTML = "Juan de los palotes 2020. La Quiaca"
+    document.getElementById("address_img").style.opacity = "1.0";
+}
+
+document.getElementById("address_img").onmouseleave = function() {
+    console.log("capturamos el evento click")
+    document.getElementById("address_img").style.opacity = "0.5";
 }
