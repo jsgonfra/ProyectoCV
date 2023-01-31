4ccfd07255cab66f0c311b97f5e4f726c5fdf9f5
Jorge Sebastian Gonzaez Franze <jsgonfra@gmail.com>
2023-01-31 18:25:26 -0300

    Ultimos cambios2: Link a google maps en nueva pestaña

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -38,7 +38,7 @@ document.getElementById("phone_img").onmouseleave = function() {
 
 document.getElementById("address_img").onmouseover = function() {
     console.log("captura el evento onmouseover")
-    document.getElementById("dato").innerHTML = "<a href='https://www.google.com/maps/place/8723+Chester+Forest+Ln,+Richmond,+VA+23237,+EE.+UU./@37.3983456,-77.4716261,17z/data=!3m1!4b1!4m5!3m4!1s0x89b10c01c77fe677:0x8a322ad249cc43b5!8m2!3d37.3983456!4d-77.4716261'>8723 Forest Ln</a>"
+    document.getElementById("dato").innerHTML = "<a href='https://www.google.com/maps/place/8723+Chester+Forest+Ln,+Richmond,+VA+23237,+EE.+UU./@37.3983456,-77.4716261,17z/data=!3m1!4b1!4m5!3m4!1s0x89b10c01c77fe677:0x8a322ad249cc43b5!8m2!3d37.3983456!4d-77.4716261' target='_blank'>8723 Forest Ln</a>"
     document.getElementById("address_img").style.opacity = "1.0";
 }
 
